6f83a104eedf203b2fda542b1d0233c1b297445d
nsokolovaaa <nastyaaasokolova@gmail.com>
2022-10-16 15:46:45 +0300

    task for scanner

---
+++ src/main/java/org/example/Day2/Task4.java
@@ -0,0 +1,29 @@
+package org.example.Day2;
+
+import java.util.Scanner;
+
+/** Реализовать программу, которая принимает на вход через консоль с помощью класса
+ Scanner, число x. Для этого числа, по формуле выше, необходимо вычислить
+ значение y.
+ (Для этих вещественных чисел x и y необходимо использовать тип double и метод
+ nextDouble() у Scanner’а соответственно, чтобы считать из консоли число x) **/
+
+public class Task4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите число x: ");
+        double x = scanner.nextDouble();
+        double y = 0;
+        if(x >= 5) {
+            y = (Math.pow(x, 2) - 10) / (x + 7);
+        }  else if (x > -3 &&  x < 5) {
+            y = (x + 3) * (Math.pow(x, 2) -2);
+        } else {
+            y = 249;
+
+        }
+        System.out.println(y);
+            }
+        }
+
+
