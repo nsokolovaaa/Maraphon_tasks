04482033721f75b63fe543f697a5bb146e322ae9
nsokolovaaa <nastyaaasokolova@gmail.com>
2022-10-24 15:37:21 +0300

    new tasks

---
+++ src/main/java/org/example/Day9/Rectangle.java
@@ -0,0 +1,21 @@
+package org.example.Day9;
+
+public class Rectangle extends Figure {
+    private double width, height;
+
+    public Rectangle( double width, double height, String color) {
+        super(color);
+        this.width = width;
+        this.height = height;
+    }
+
+    @Override
+    public double area() {
+        return width * height;
+    }
+
+    @Override
+    public double perimeter() {
+        return 2 * (width + height);
+    }
+}
