587cf8d61d1b4ecaf9b37cb40ae974ece41c7a84
nsokolovaaa <nastyaaasokolova@gmail.com>
2022-10-30 12:41:23 +0300

    new tasks

---
+++ src/main/java/org/example/Day16/Task1.java
@@ -0,0 +1,38 @@
+package org.example.Day16;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+/** 1. Реализовать программу, которая на вход принимает txt файл с целыми числами
+ (можно использовать файл из задания 1 дня 14) и в качестве ответа выводит в
+ консоль среднее арифметическое этих чисел.
+ Ответ будет являться вещественным числом. В консоль необходимо вывести полную
+ запись вещественного числа (со всеми знаками после запятой) и сокращенную запись
+ (с 3 знаками после запятой).
+ Детали реализации: В классе Task1 создать публичный статический метод
+ printResult(File file), в котором реализовать вышеописанную логику. В методе
+ main() класса Task1 вызвать метод printResult(File file), передав ему в
+ качестве аргумента объект класса File (txt файл с целыми числами).
+ Пример:
+ Числа в txt файле: 5 2 8 34 1 36 77
+ Ответ: 23.285714285714285 --> 23,286
+
+ ...  **/
+public class Task1 {
+    public static void main(String[] args) throws FileNotFoundException {
+        File file = new File("C:\\Users\\USER\\Desktop\\Maraphon_tasks\\src\\main\\java\\org\\example\\input.txt");
+        Scanner scanner = new Scanner(file);
+        String scan = scanner.nextLine();
+        String[] line = scan.split(" ");
+
+        int sum = 0;
+        for(String number : line) {
+            sum += Integer.parseInt(number);
+            double result = sum / line.length;
+            System.out.printf(result + " ---> %.3f", result);
+        }
+
+    }
+    }
